7cba29fab9adef4867d424489d426b72a24bd029
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-01-12 20:14:34 +0044

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,41 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
 function Listing() {
     return (
-        <Pagination />
- 
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    {/*    ocupa 6 colunas das 12 colunas do grid system do bootstrap  */}
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
+
+
+        </>
     );
 }
 
