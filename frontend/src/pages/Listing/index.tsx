66daf144f57da99cc98a63a78a546ba7256a46c1
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-01-16 11:18:54 +0044

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,34 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_url } from "utils/requests";
 
 function Listing() {
 
-axios
-     .get(`${BASE_url}/movies?size=10&page=0`)
-     .then( 
-            resposta => {
-                console.log(resposta.data);
-            }
-     )
-     .catch( e => console.log(e) );
+    // em português, fora do padrão, para lembrar a função useState
+    //default - 0 
+    const [numeroPagina , setNumeroPagina] =useState(0);
+ 
+     useEffect( () => {
+    // executa apenas uma vez de acordo com ciclo de vida.
+        axios
+        .get(`${BASE_url}/movies?size=10&page=0`)
+        .then( 
+               resposta => {
+                    const data = resposta.data as MoviePage
+                    setNumeroPagina(data.number)
+                   console.log(data.number);
+                   console.log(data);
+               }
+        )
+        .catch( e => console.log(e) );
+
+     }, []);
+
     return (
-        <>
+        <> 
             <Pagination />
             <div className="container">
                 <div className="row">
