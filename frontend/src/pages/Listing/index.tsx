6e3c37356240534616baa0376308471a66529a56
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-01-16 11:39:40 +0044

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,54 +7,63 @@ import { BASE_url } from "utils/requests";
 
 function Listing() {
 
-    // em português, fora do padrão, para lembrar a função useState
-    //default - 0 
-    const [numeroPagina , setNumeroPagina] =useState(0);
- 
-     useEffect( () => {
-    // executa apenas uma vez de acordo com ciclo de vida.
-        axios
-        .get(`${BASE_url}/movies?size=10&page=0`)
-        .then( 
-               resposta => {
-                    const data = resposta.data as MoviePage
-                    setNumeroPagina(data.number)
-                   console.log(data.number);
-                   console.log(data);
-               }
-        )
-        .catch( e => console.log(e) );
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    }
 
-     }, []);
+
+    // em português, fora do padrão, para lembrar a função useState
+    // manter estado na aplicação.
+    const [numeroPagina, setNumeroPagina] = useState(0);
+
+    //setando usestate default para MoviePage
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+
+    });
+
+    useEffect(() => {
+        // executa apenas uma vez de acordo com ciclo de vida. init - destroy
+        axios
+            .get(`${BASE_url}/movies?size=10&page=${numeroPagina}&sort=title`)
+            .then(
+                resposta => {
+                    //resposta da api
+                    const data = resposta.data as MoviePage
+                    //setando  useState MoviePage
+                    setPage(data)
+                }
+            )
+            .catch(e => console.log(e));
+
+    }, [numeroPagina]);
 
     return (
-        <> 
+        <>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    {/*    ocupa 6 colunas das 12 colunas do grid system do bootstrap  */}
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(item => (
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                        //   {/*    ocupa 6 colunas das 12 colunas do grid system do bootstrap  */ }
+                        < div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                            <MovieCard movie={item} key={item.id} />
+                        </div>
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    )
+                    )}
 
                 </div>
             </div>
